f47dfaba3ef97094d4e20f541e7fc791ff0d1a62
Chao Jiang <co.jiaan@proton.me>
2022-12-13 19:03:38 +0800

    auto push 2022/12/13@19:03:38

---
+++ Shell/Logic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+a=10
+b=-5
+c=100
+
+if [[ a -gt 1 ]] && [[ c -gt 1 ]]; then
+	echo "they are both greater than one"
+else
+	echo "they are not both greater than one"
+fi
---
+++ Shell/nest_logic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ 3 -gt 0 ]] && [[ 3 -lt 10 ]];
+then
+	echo "That's right.\n My bro ！"
+else
+	echo "you missed \n Try again."
+fi
